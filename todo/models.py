1898e1d749bd7fe81c5ef63116c3b63bd8bc9f93
Radhika Patel <radhika.patel2206@gmail.com>
2020-05-11 09:39:15 +0530

    project prep

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Todolist(models.Model):
+    item = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    done = models.BooleanField(default=False)
+    completed_on = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.item
